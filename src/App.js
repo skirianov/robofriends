3f7c47e2020266452f2fff655d9fb9e17e9952a0
S.Kiri <s.kirianov@gmail.com>
2021-01-08 15:19:22 +0100

    Added scrollable segment

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
-import { robots } from './robots';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 import './App.css';
+import { robots } from './robots';
 
 class App extends Component{
 
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchField: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users }))
     }
 
     onSearchChange = (event) => {
@@ -24,14 +31,22 @@ class App extends Component{
         const filteredRobots = this.state.robots.filter ( each => {
             return each.name.toLowerCase().includes(this.state.searchField.toLowerCase())
         });
-    return (
-        <div className="tc">
-            <h1 className="f1">Robofriends</h1>
-            <SearchBox searchChange={this.onSearchChange}/>
-            <CardList robots={filteredRobots }/>
-        </div>
-    );
+
+        if (this.state.robots.length === 0) {
+            return <h1>Loading</h1>
+        } else {
+            return (
+                <div className="tc">
+                <h1 className="f1">Robofriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <Scroll>
+                    <CardList robots={filteredRobots }/>
+                </Scroll>
+                </div>
+            );
+        }
     }
+   
 }
  
 export default App;
--- src/card.js
+++ src/card.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const Card = ({ name, email, id }) => {
+const Card = ({ name, email, id, website}) => {
     return (
         <div className = "tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
             <img src={`https://robohash.org/${id}?100x100`} alt="robots"/>
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
+                <a href={`https://${website}`}>{`${website}`}</a>
             </div>
         </div>
     )
